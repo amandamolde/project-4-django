f8844ed63f0ca1bfa6bbd55bc1e143680e4de8bd
amandamolde <amanda.molde@gmail.com>
2018-08-17 11:24:58 -0600

    fixed errors

--- blog_django/blog/models.py
+++ blog_django/blog/models.py
@@ -4,11 +4,11 @@ from django.db import models
 class Post(models.Model):
     date = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
-    body = models.CharField(max_length=100)
-    img_url = models.TextField()
+    body = models.TextField()
+    img_url = models.TextField(blank=True)
 
     def __str__(self):
-        return self
+        return self.title
 
 
 class Comment(models.Model):
@@ -17,4 +17,4 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
 
     def __str__(self):
-        return self
+        return self.body
