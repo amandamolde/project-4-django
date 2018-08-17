d3cb56e759db43d493a95a797c0a177dd69bd295
amandamolde <amanda.molde@gmail.com>
2018-08-17 10:30:46 -0600

    updated small urls

--- blog_django/blog/urls.py
+++ blog_django/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.DetailPost.as_view()),
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('<int:pk>/comments/', views.CommentList.as_view()),
     path('<int:pk>/comments/<int:pk>/', views.CommentDetail.as_view()),
 ]
