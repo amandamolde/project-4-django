f23073216adab5b9639187d255432eb5193ff22d
amandamolde <amanda.molde@gmail.com>
2018-08-17 15:03:56 -0600

    changed smallURLS to REST type

--- blog_django/blog/urls.py
+++ blog_django/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>/', views.PostDetail.as_view()),
-    path('posts/<int:pk>/comments/', views.CommentList.as_view()),
-    path('posts/<int:pk>/comments/<int:id>/', views.CommentDetail.as_view()),
+    path('posts/<int:pk>', views.PostDetail.as_view()),
+    path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>', views.CommentDetail.as_view()),
 ]
